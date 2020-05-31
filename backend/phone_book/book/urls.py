bec1ca58b6f928ea71e1f31e436c0499f2a192a0
Avaaren <avaaren@mail.ru>
2020-05-31 18:05:58 +0300

    Added urls for person and phone edit

--- backend/phone_book/book/urls.py
+++ backend/phone_book/book/urls.py
@@ -3,5 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.PersonView.as_view())
+    path('', views.PersonListView.as_view()),
+    path('<int:person_id>', views.PersonEditView.as_view()),
+    path('<int:person_id>/<int:number_id>/', views.NumberEditView.as_view()),
 ]
