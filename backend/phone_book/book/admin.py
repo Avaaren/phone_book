49da481149583846ae25f4a90c74e62ea07fcea5
Avaaren <avaaren@mail.ru>
2020-05-31 15:16:46 +0300

    Registered models on admin site

--- backend/phone_book/book/admin.py
+++ backend/phone_book/book/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import (
+    Person,
+    PhoneNumber
+)
+
+@admin.register(Person)
+class PersonAdmin(admin.ModelAdmin):
+    
+    class Meta:
+        list_display = '__all__'
+
+@admin.register(PhoneNumber)
+class PhoneNumberAdmin(admin.ModelAdmin):
+    
+    class Meta:
+        list_display = '__all__'
