51fcfd914209c977f8a1097e8f6e094728f2f14b
Avaaren <avaaren@mail.ru>
2020-05-31 15:17:04 +0300

    Added get req handling in main page view

--- backend/phone_book/book/views.py
+++ backend/phone_book/book/views.py
@@ -1,3 +1,22 @@
 from django.shortcuts import render
 
-# Create your views here.
+from rest_framework.views import APIView
+from rest_framework.response import Response
+
+from .serializers import (
+    PersonSerializer,
+    PhoneNumberSerializer
+)
+
+from .models import (
+    Person,
+    PhoneNumber
+)
+
+
+class PersonView(APIView):
+
+    def get(self, request):
+        persons = Person.objects.all()
+        serializer = PersonSerializer(persons, many=True)
+        return Response(serializer.data)
