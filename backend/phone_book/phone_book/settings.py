3df0c02f11e5cbc19f68019fcb9c7b3043cf8eb6
Avaaren <avaaren@mail.ru>
2020-06-01 13:52:59 +0300

    Added settings for celery

--- backend/phone_book/phone_book/settings.py
+++ backend/phone_book/phone_book/settings.py
@@ -1,5 +1,5 @@
 import os
-
+from celery.schedules import crontab
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -105,10 +105,11 @@ TIME_ZONE = 'UTC'
 
 USE_I18N = True
 
-USE_L10N = True
+USE_L10N = False
 
 USE_TZ = True
 
+DATETIME_FORMAT = 'Y-m-d H:i:s'
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
@@ -117,3 +118,17 @@ STATIC_URL = '/static/'
 
 CORS_ORIGIN_ALLOW_ALL = False
 CORS_ORIGIN_WHITELIST = ('https://localhost:3000',)
+
+# Celery
+CELERY_BROKER_URL = 'redis://localhost:6379'  
+CELERY_RESULT_BACKEND = 'redis://localhost:6379'  
+CELERY_ACCEPT_CONTENT = ['application/json']  
+CELERY_RESULT_SERIALIZER = 'json'  
+CELERY_TASK_SERIALIZER = 'json' 
+
+CELERY_BEAT_SCHEDULE = {
+    'update_number_of_commits': {
+        'task': 'github.tasks.update_number_of_commits',
+        'schedule': crontab(hour='*/6'),
+    }
+}
