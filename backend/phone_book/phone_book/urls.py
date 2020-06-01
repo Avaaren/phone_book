83c6735dc4ddad3fe42eb4bfde8cf02aba848fe3
Avaaren <avaaren@mail.ru>
2020-06-01 12:35:22 +0300

    Added url for weather app

--- backend/phone_book/phone_book/urls.py
+++ backend/phone_book/phone_book/urls.py
@@ -4,5 +4,6 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('book.urls')),
+    path('api/weather/', include('weather.urls')),
     path('api_auth/', include('rest_framework.urls', namespace='rest_framework')),
 ]
