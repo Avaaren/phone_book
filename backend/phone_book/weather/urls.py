8d1505e1f5858203885df438740390a916e9a1ab
Avaaren <avaaren@mail.ru>
2020-06-01 14:25:48 +0300

    Added url for interact with cities

--- backend/phone_book/weather/urls.py
+++ backend/phone_book/weather/urls.py
@@ -3,5 +3,5 @@ from . import views
 urlpatterns = [
     path('', views.DisplayWeatherView.as_view()),
     path('add_city/', views.AddCityView.as_view()),
-    # path('delete_city/', views.DeleteCityView.as_view()),
+    path('<int:pk>/', views.DeleteCityView.as_view()),
 ]
