0701fbe6b804d16f7cf16bb3911a221538c76285
Avaaren <avaaren@mail.ru>
2020-06-01 13:50:36 +0300

    Added url for add city view

--- backend/phone_book/weather/urls.py
+++ backend/phone_book/weather/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 urlpatterns = [
     path('', views.DisplayWeatherView.as_view()),
-    # path('add_city/', views.AddCityView.as_view()),
+    path('add_city/', views.AddCityView.as_view()),
     # path('delete_city/', views.DeleteCityView.as_view()),
 ]
