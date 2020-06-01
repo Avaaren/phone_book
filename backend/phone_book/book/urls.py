4b55d3e41562f2db53b05cd4ea68d62d36b90f39
Avaaren <avaaren@mail.ru>
2020-06-01 11:12:10 +0300

    Added url for adding number for certain person

--- backend/phone_book/book/urls.py
+++ backend/phone_book/book/urls.py
@@ -5,5 +5,6 @@ from . import views
 urlpatterns = [
     path('', views.PersonListView.as_view()),
     path('<int:person_id>', views.PersonEditView.as_view()),
+    path('<int:person_id>/add_number/', views.PersonAddNumberView.as_view()),
     path('<int:person_id>/<int:number_id>/', views.NumberEditView.as_view()),
 ]
