c68ee357e38657767c03a5343109c00311ac5b4c
Avaaren <avaaren@mail.ru>
2020-06-01 11:12:47 +0300

    Added view for adding number for certain person. Added docstrings

--- backend/phone_book/book/views.py
+++ backend/phone_book/book/views.py
@@ -3,7 +3,10 @@ from django.shortcuts import get_object_or_404
 from django.http import Http404
 
 from rest_framework.views import APIView
-from rest_framework.generics import RetrieveUpdateDestroyAPIView, ListCreateAPIView
+from rest_framework.generics import (
+    RetrieveUpdateDestroyAPIView,
+    ListCreateAPIView
+)
 from rest_framework import status
 from rest_framework.response import Response
 
@@ -19,16 +22,18 @@ from .models import (
 
 
 class PersonListView(ListCreateAPIView):
+    '''Отображение и создание пользователя'''
     queryset = Person.objects.all()
     serializer_class = PersonSerializer
 
     def get(self, request):
+        '''Отображение списка всех пользователей'''
         persons = Person.objects.all()
         serializer = PersonSerializer(persons, many=True)
         return Response(serializer.data)
+
     def post(self, request):
-        # Create an article from the above data
-        # print(request.data)
+        '''Создание нового пользователя с одним или несколькими телефонами'''
         serializer = PersonSerializer(data=request.data)
         
         if serializer.is_valid():
@@ -40,6 +45,7 @@ class PersonListView(ListCreateAPIView):
 
 
 class PersonEditView(RetrieveUpdateDestroyAPIView):
+    '''Редактирование и удаление абонента'''
     queryset = Person.objects.all()
     serializer_class = PersonSerializer
 
@@ -48,15 +54,28 @@ class PersonEditView(RetrieveUpdateDestroyAPIView):
         return get_object_or_404(Person, pk=pk)
 
 
+class PersonAddNumberView(ListCreateAPIView):
+    '''Добавление номера для абонента'''
+    serializer_class = PhoneNumberSerializer
+
+    def get_queryset(self):
+        person_id = self.kwargs.get('person_id')
+        owner = get_object_or_404(Person, pk=self.kwargs.get('person_id'))
+        return PhoneNumber.objects.filter(owner=owner)
+
+    def perform_create(self, serializer):
+        owner = get_object_or_404(Person, pk=self.kwargs.get('person_id'))
+        return serializer.save(owner=owner)
+
+
 class NumberEditView(RetrieveUpdateDestroyAPIView):
-    # queryset = Person.objects.all()
+    '''Редактирование и удаление номера телефона'''
     serializer_class = PhoneNumberSerializer
 
     def get_object(self):
         person_pk = self.kwargs.get('person_id')
         number_pk = self.kwargs.get('number_id')
-        try:
-            number = Person.objects.get(pk=person_pk).phones.get(pk=number_pk)
-        except PhoneNumber.DoesNotExist:
-            raise Http404('Lol')
+
+        owner = get_object_or_404(Person, pk=person_pk)
+        number = get_object_or_404(PhoneNumber, pk=person_pk, owner=owner)
         return number
