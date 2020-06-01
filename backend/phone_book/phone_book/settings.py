90000793e363aeb6dfae4065ddeeb3665d81c4a2
Avaaren <avaaren@mail.ru>
2020-06-01 11:13:04 +0300

    Added settings for corsheaders

--- backend/phone_book/phone_book/settings.py
+++ backend/phone_book/phone_book/settings.py
@@ -36,12 +36,12 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
-    'corsheaders.middleware.CorsMiddleware'
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware'
 ]
 
 ROOT_URLCONF = 'phone_book.urls'
@@ -115,4 +115,4 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 CORS_ORIGIN_ALLOW_ALL = False
-CORS_ORIGIN_WHITELIST('localhost:3000',)
+CORS_ORIGIN_WHITELIST = ('https://localhost:3000',)
